9533526b27b1bd73a21dc4316929ae0936fe65c7
Juan Carlos Garcia López <jcgarcialo@montepiedad.com.mx>
2021-08-18 14:49:27 -0500

    Se guarda base del proyecto.

---
+++ src/main/java/mx/com/nmp/msIntermediarioRedis/MsIntermediarioRedisApplication.java
@@ -0,0 +1,13 @@
+package mx.com.nmp.msIntermediarioRedis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsIntermediarioRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsIntermediarioRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/nmp/msIntermediarioRedis/MsIntermediarioRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.nmp.msIntermediarioRedis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsIntermediarioRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
