a3304df0694dc57bddfecb588cefa3b98fa505ab
Juan Carlos Garcia López <jcgarcialo@montepiedad.com.mx>
2021-09-23 18:27:36 -0500

    Se cambia implementación de conexión con redis.

--- src/service/partidas.service.js
+++ src/service/partidas.service.js
@@ -7,16 +7,10 @@ if (url === undefined) {
   process.exit(1)
 }
 
-const certBase64 = process.env.REDIS_CERT_BASE64
-if (certBase64 === undefined) {
-  LOG.error('Es necesaria la variable: REDIS_CERT_BASE64')
-  process.exit(1)
-}
 
-const ca = Buffer.from(certBase64, 'base64').toString('utf-8')
-const tls = { ca }
 
-const client = redis.createClient(url, { tls })
+const client = redis.createClient(url)
+
 
 client.on('connect', function onConect() {
   LOG.info(`Redis connected!!! on ${url}`)
