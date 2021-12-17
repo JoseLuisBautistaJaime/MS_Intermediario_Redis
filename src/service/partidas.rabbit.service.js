d9196f2dcd87e0bca7b6b150ccd714881d6ad2f3
Juan Carlos Garcia López <jcgarcialo@montepiedad.com.mx>
2021-12-17 13:06:30 -0600

    Se guarda cambio para encolar pagos.

--- src/service/partidas.rabbit.service.js
+++ src/service/partidas.rabbit.service.js
@@ -4,26 +4,7 @@ import { AMQP_MESSAGES_QUEUE } from '../constants'
 const sender = require('./rabbit-queuesender.service')
 const { messageRabbitDao } = require('./rabbit.service')
 
-const procesaMensaje = async messageRecieved => {
-  LOG.debug('SERVICE: Starting procesaMensaje method')
-  LOG.debug(`messageRecieved - ${JSON.stringify(messageRecieved)}`)
-  const { idMensaje, cuerpoMensaje } = messageRecieved.message
-  LOG.trace('idMensaje', idMensaje)
-  LOG.trace('cuerpoMensaje', cuerpoMensaje)
-  LOG.debug('SERVICE: Starting procesaMensaje method')
-}
-
-const desencolaMensaje = async message => {
-  LOG.debug('SERVICE: Starting desencolaMensaje memthod')
-
-  const messageRecieved = JSON.parse(message.content.toString())
-  LOG.traceJSON('---->messageRecieved', messageRecieved)
-  messageRabbitDao.ackMessage(message)
-  await procesaMensaje(messageRecieved)
-  LOG.debug('SERVICE: Ending desencolaMensaje method')
-}
-
-const encolaPago = async (idMensaje, cuerpoMensaje) => {
+const encolaMensaje = async (idMensaje, cuerpoMensaje) => {
   LOG.debug('SERVICE: Starting encolaPago method')
   const queueMessage = {
     queue: AMQP_MESSAGES_QUEUE,
@@ -37,8 +18,26 @@ const encolaPago = async (idMensaje, cuerpoMensaje) => {
   LOG.debug('SERVICE: Ending encolaPago method')
 }
 
+const procesaMensaje = async messageRecieved => {
+  LOG.debug('SERVICE: Starting procesaMensaje method')
+  LOG.debug(`messageRecieved - ${JSON.stringify(messageRecieved)}`)
+  const { idMensaje, cuerpoMensaje } = messageRecieved.message
+  LOG.trace('idMensaje', idMensaje)
+  LOG.trace('cuerpoMensaje', cuerpoMensaje)
+  LOG.debug('SERVICE: Starting procesaMensaje method')
+}
+
+const desencolaMensaje = async message => {
+  LOG.debug('SERVICE: Starting desencolaMensaje memthod')
+  const messageRecieved = JSON.parse(message.content.toString())
+  LOG.traceJSON('messageRecieved', messageRecieved)
+  messageRabbitDao.ackMessage(message)
+  await procesaMensaje(messageRecieved)
+  LOG.debug('SERVICE: Ending desencolaMensaje method')
+}
+
 export const PartidasRabbitService = {
-  encolaPago,
+  encolaMensaje,
   desencolaMensaje
 }
 
